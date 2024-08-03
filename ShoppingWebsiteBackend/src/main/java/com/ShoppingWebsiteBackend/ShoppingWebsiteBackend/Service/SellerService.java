6749637ea9408f35665dbbaad1546945bd5a2616
immeghana <immeghana@example.com>
2024-08-04 00:06:50 +0530

    remove product code done

--- ShoppingWebsiteBackend/src/main/java/com/ShoppingWebsiteBackend/ShoppingWebsiteBackend/Service/SellerService.java
+++ ShoppingWebsiteBackend/src/main/java/com/ShoppingWebsiteBackend/ShoppingWebsiteBackend/Service/SellerService.java
@@ -7,13 +7,9 @@ import com.ShoppingWebsiteBackend.ShoppingWebsiteBackend.exception.InvalidProduc
 import com.ShoppingWebsiteBackend.ShoppingWebsiteBackend.exception.UserNotFound;
 import com.ShoppingWebsiteBackend.ShoppingWebsiteBackend.model.AppUser;
 import com.ShoppingWebsiteBackend.ShoppingWebsiteBackend.model.Product;
-import com.ShoppingWebsiteBackend.ShoppingWebsiteBackend.responsebody.ProductResponseBody;
-import com.ShoppingWebsiteBackend.ShoppingWebsiteBackend.responsebody.UserResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -94,34 +90,5 @@ public class SellerService {
                 owner.getName(),
                 product.getProductName()
         );
-    }
-
-    public List<ProductResponseBody> getAllProductsBySellerID(UUID sellerID) {
-        List<Product> products = productRepository.getAllProductsBySellerID(sellerID);
-        List<ProductResponseBody> productResponseBodies = new ArrayList<>();
-
-        for(Product product : products){
-            AppUser seller = product.getSeller();
-            ProductResponseBody productResponseBody = new ProductResponseBody();
-            UserResponse userResponse=new UserResponse();
-            // Setting product values inside the product response body
-            productResponseBody.setProductName(product.getProductName());
-            productResponseBody.setCategory(product.getProductCategory());
-            productResponseBody.setPrice(product.getPrice());
-            productResponseBody.setRating(product.getRating());
-            productResponseBody.setQuantity(product.getQuantity());
-            // Setting values inside the user response body.
-            userResponse.setUserName(seller.getName());
-            userResponse.setEmail(seller.getEmail());
-            userResponse.setPhoneNumber(seller.getPhoneNumber());
-            userResponse.setAddress(seller.getAddress());
-            userResponse.setAge(seller.getAge());
-            // Set userResponseBody inside product response body.
-            productResponseBody.setUserResponse(userResponse);
-
-            // For a specific product, product response body is built. Now we need to add each product response body into list.
-            productResponseBodies.add(productResponseBody);
-        }
-        return productResponseBodies;
     }
 }
